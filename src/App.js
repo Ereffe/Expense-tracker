416f3d110e57ec18d30bb6c51ec86c675610257b
Kacper Mirocha <74498395+Ereffe@users.noreply.github.com>
2023-08-24 16:06:50 +0200

    Added a new placeholder component. Added css for that component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpeseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpeseItem></ExpeseItem>
     </div>
   );
 }
