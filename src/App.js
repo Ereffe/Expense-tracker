63eb7e97cc7d2f51c3c6b4a74b5f7f4ff3d4f4ea
Kacper Mirocha <74498395+Ereffe@users.noreply.github.com>
2023-08-24 18:13:22 +0200

    Changed way of passing props to ExpenseItem from App.js

--- src/App.js
+++ src/App.js
@@ -31,26 +31,10 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpeseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpeseItem>
-      <ExpeseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpeseItem>
-      <ExpeseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpeseItem>
-      <ExpeseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpeseItem>
+      <ExpeseItem expense={expenses[0]}></ExpeseItem>
+      <ExpeseItem expense={expenses[1]}></ExpeseItem>
+      <ExpeseItem expense={expenses[2]}></ExpeseItem>
+      <ExpeseItem expense={expenses[3]}></ExpeseItem>
     </div>
   );
 }
